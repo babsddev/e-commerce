a49f9fd39f4fed4aa2cf8ad6e5b2c151502d8d5b
babsddev <ajayi.gbemi0806@gmail.com>
2022-10-15 11:09:05 +0100

    e-commerce

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Product from './pages/Product'
+// import Product from './pages/Product'
 import Home from './pages/Home'
-import ProductList from './pages/ProductList'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Cart from './pages/Cart'
+// import ProductList from './pages/ProductList'
+// import Register from './pages/Register'
+// import Login from './pages/Login'
+// import Cart from './pages/Cart'
 
 const App = () => {
   return (
@@ -13,7 +13,7 @@ const App = () => {
       {/* <ProductList /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      <Cart />
+      {/* <Cart /> */}
     </>
   )
 }
